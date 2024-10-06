290e188bd4d764ae535d333e3072c12cd526f48e
krivacevicmarko <krivacevic.marko.98@gmail.com>
2024-10-06 15:12:13 +0200

    Delete Regex Validator directory

--- Regex Validator/src/main/java/rs/raf/m_stojanovic/pp/programs/regexvalidator/Main.java
+++
@@ -1,7 +0,0 @@
-package rs.raf.m_stojanovic.pp.programs.regexvalidator;
-
-public class Main {
-    public static void main(String[] args) {
-        App.launch(App.class, args);
-    }
-}
--- Regex Validator/src/main/java/rs/raf/m_stojanovic/pp/programs/regexvalidator/model/RegexSource.java
+++
@@ -1,4 +0,0 @@
-package rs.raf.m_stojanovic.pp.programs.regexvalidator.model;
-
-public interface RegexSource {
-}
